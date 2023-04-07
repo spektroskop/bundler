7f5f2cdf9376512c839392438fbb7e4c677aeca2
Alexander Stensrud <alex@yuki.no>
2023-04-07 20:37:04 +0200

    Split meta

--- internal/meta/meta.go
+++ internal/meta/meta.go
@@ -22,55 +22,65 @@ func New(save string) api.Plugin {
 	return api.Plugin{Name: "meta", Setup: setup(save)}
 }
 
-func setup(save string) func(build api.PluginBuild) {
+func setup(path string) func(build api.PluginBuild) {
 	return func(build api.PluginBuild) {
-		build.OnEnd(onEnd(save))
+		build.OnEnd(onEnd(path))
 	}
 }
 
-func onEnd(save string) func(result *api.BuildResult) (api.OnEndResult, error) {
-	return func(result *api.BuildResult) (api.OnEndResult, error) {
+func onEnd(path string) func(result *api.BuildResult) (api.OnEndResult, error) {
+	return func(result *api.BuildResult) (r api.OnEndResult, _ error) {
 		if len(result.Metafile) == 0 {
-			return api.OnEndResult{}, nil
+			return r, nil
 		}
 
+		data := []byte(result.Metafile)
 		var meta Meta
-		if err := json.Unmarshal([]byte(result.Metafile), &meta); err != nil {
-			return api.OnEndResult{}, err
-		}
-
-		if save != "" {
-			f, err := os.Create(save)
-			if err != nil {
-				return api.OnEndResult{}, err
-			}
-
-			if _, err := f.Write([]byte(result.Metafile)); err != nil {
-				return api.OnEndResult{}, err
-			}
-
-			log.Info().Str("path", save).Msg("saved meta file")
+		if err := json.Unmarshal(data, &meta); err != nil {
+			return r, err
 		}
 
 		for name, output := range meta.Outputs {
-			var source []string
-
-			for name := range output.Inputs {
-				if parts := strings.Split(name, ":"); len(parts) == 2 {
-					if wd, err := os.Getwd(); err == nil {
-						if path, err := filepath.Rel(wd, parts[1]); err == nil {
-							source = append(source, path)
-							continue
-						}
-					}
-				}
-
-				source = append(source, name)
-			}
-
-			log.Info().Strs("source", source).Msg(name)
+			log.Info().Strs("source", source(output)).Msg(name)
 		}
 
-		return api.OnEndResult{}, nil
+		return r, save(path, data)
 	}
+}
+
+func source(output Output) (source []string) {
+	for name := range output.Inputs {
+		if parts := strings.Split(name, ":"); len(parts) == 2 {
+			name = parts[1]
+		}
+
+		if wd, err := os.Getwd(); err == nil {
+			if path, err := filepath.Rel(wd, name); err == nil {
+				source = append(source, path)
+				continue
+			}
+		}
+
+		source = append(source, name)
+	}
+
+	return source
+}
+
+func save(path string, data []byte) error {
+	if path == "" {
+		return nil
+	}
+
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	if _, err := f.Write(data); err != nil {
+		return err
+	}
+
+	log.Info().Str("path", path).Msg("saved meta file")
+	return nil
 }
