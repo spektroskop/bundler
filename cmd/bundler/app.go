575f3520d0097e836df9603b07e5b9d7986e87ac
Alexander Stensrud <alex@yuki.no>
2023-04-11 04:50:53 +0200

    Use `--set` for `--resolve`

--- cmd/bundler/app.go
+++ cmd/bundler/app.go
@@ -14,8 +14,7 @@ type App struct {
 	Activate    []string          `help:"List of optional plugins to activate (${enum})." enum:"tailwind" placeholder:"NAME"`
 	Deactivate  []string          `help:"List of plugins to deactivate (${enum})." enum:"elm,gleam,gren" placeholder:"NAME"`
 	Loader      map[string]Loader `help:"Loaders (jsx,file)." placeholder:"EXT:NAME"`
-	Resolve     map[string]string `help:"Plugin resolve path." placeholder:"PLUGIN=PATH"`
-	Set         map[string]string `help:"Set config values." placeholder:"KEY=VALUE" mapsep:","`
+	Config      map[string]string `help:"Set config values." name:"set" placeholder:"KEY=VALUE" mapsep:","` // TODO: List config keys in help text
 }
 
 type Loader api.Loader
